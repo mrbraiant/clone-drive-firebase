84009d2a5a6f7688ade9f0d56b1b0a00b9054380
mrbraiant <mrbraiant@gmail.com>
2023-07-27 01:48:41 +0100

    build: creating sidebar, fixing navbar

--- src/home.js
+++ src/home.js
@@ -1,37 +1,85 @@
-import { Search } from '@mui/icons-material';
-import { InputAdornment, TextField } from '@mui/material';
+import { Search, Tune, CheckCircleOutline, HelpOutline, Settings, Apps, Add } from '@mui/icons-material';
+import { Button, IconButton, InputAdornment, Paper, TextField, Typography } from '@mui/material';
 import React from 'react';
 import './home.css';
-// import Search from '@material-design-icons/svg/outlined/search.svg';
+
+
 
 function Home(props) {
     return (
-        <header className='header'>
+        <div className='content'>
             <div className='navbar'>
                 <div className='logo'>
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/2295px-Google_Drive_icon_%282020%29.svg.png" alt="logo" />
                     <h2>Drive</h2>
                 </div>
 
-                <TextField
-                    id="input-with-icon-textfield"
-                    // label="TextField"
-                    InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <Search />
-                        </InputAdornment>
-                    ),
-                    }}
-                    // variant="standard"
-                />
+                <div style={{display: 'flex', flexDirection: 'row', width: 'calc(100% - 256px)', justifyContent: 'space-between'}}>
+                    <TextField
+                        id="input-with-icon-textfield"
+                        // label="TextField"
+                        size='small'
+                        placeholder='Search on Drive'
+                        InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <Search />
+                            </InputAdornment>
+                        ),
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <Tune />
+                            </InputAdornment>
+                        ),
+                        }}
+                        // fullWidth
+                        sx={{
+                            width: '100%',
+                            maxWidth: '722px',
+                            backgroundColor: '#EDF2FC',
+                            borderRadius: 100,
+                            [`& fieldset`]: {
+                                border: 0,
+                                borderRadius: 100,
+                            },
+                        }}
+                        // variant="standard"
+                    />
 
-                {/* <input type="text" placeholder="Search on Drive" /> */}
-                <div className='profile'>
-                    <img src={props.login.image} alt="profile" data-s />
+                    <div className='icons-box'>
+                        <IconButton>
+                            <CheckCircleOutline />
+                        </IconButton>
+                        <IconButton>
+                            <HelpOutline />
+                        </IconButton>
+                        <IconButton>
+                            <Settings />
+                        </IconButton>
+                        <IconButton>
+                            <Apps />
+                        </IconButton>
+                    </div>
+
+                    <div className='profile'>
+                        <img src={props.login.image} alt="profile" />
+                    </div>
                 </div>
+                
             </div>
-        </header>
+            <div className='sidebar'>
+                <form>
+                    <Button variant='contained' sx={{width: '106px', height: '56px', padding: '18px 20px', borderRadius: '15px', display: 'flex', alignItems: 'center', gap: '0.5rem', cursor: 'pointer', backgroundColor: '#fff', color: '#444746', "&:hover":{ backgroundColor: "#E7EDF9"}, textTransform: "none"}}>
+                        <Add />
+                        <label for='attachment'>Novo</label>
+                        <input className='hidden-input' type='file' id='attachment' name='attachment' />
+                    </Button>
+                </form>
+                <Typography>Folder 1</Typography>
+                <Typography>Folder 2</Typography>
+                <Typography>Folder 3</Typography>
+            </div>
+        </div>
     )
 }
 
